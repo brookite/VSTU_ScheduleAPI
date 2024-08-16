67a7a2a9b717c6c282728093b18faa24e37eadc7
Dmitry Sh (Brookit) <brookit18@gmail.com>
2024-08-16 16:12:04 +0300

    Изменена структура базы данных (опять)

--- api/models.py
+++ api/models.py
@@ -3,8 +3,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
-from api.service_accounts import service_account
-
 
 class CommonModel(models.Model):
     class Meta:
@@ -15,6 +13,7 @@ class CommonModel(models.Model):
     datemodified = models.DateTimeField(auto_now_add=True, verbose_name="Дата изменения записи")
     dateaccessed = models.DateTimeField(null=True, blank=True, verbose_name="Дата доступа к записи")
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    note = models.TextField(null=True, blank=True, verbose_name="Комментарий для этой записи", max_length=1024)
 
     @classmethod
     def last_modified_record(cls) -> Self:
@@ -26,7 +25,7 @@ class Subject(CommonModel):
         verbose_name = "Предмет"
         verbose_name_plural = "Предметы"
     
-    name = models.CharField(max_length=256, null=False, blank=False)
+    name = models.CharField(max_length=256)
 
 
 class TimeSlot(CommonModel):
@@ -35,10 +34,10 @@ class TimeSlot(CommonModel):
         verbose_name_plural = "Времена проведения события"
 
     start_time = models.TimeField(verbose_name="Время начала")
-    end_time = models.TimeField(verbose_name="Время окончания")
+    end_time = models.TimeField(null=True, verbose_name="Время окончания")
 
     def clean(self):
-        if self.end_time <= self.start_time:
+        if self.end_time and self.end_time <= self.start_time:
             raise ValidationError("Время проведения не корректно")
 
 
@@ -65,19 +64,21 @@ class EventParticipant(CommonModel):
     role = models.CharField(choices=Role, max_length=48, null=False)
 
 
+class EventKind(CommonModel):
+    class Meta:
+        verbose_name = "Тип события"
+        verbose_name_plural = "Типы событий"
+
+    name = models.CharField(verbose_name="Название типа", max_length=64)
+
+
 class Event(CommonModel):
     class Meta:
         verbose_name = "Событие"
         verbose_name_plural = "События"
 
-    class Kind(models.TextChoices):
-        LECTURE = "LEC", "Лекционное занятие"
-        LABWORK = "LAB", "Лабораторная работа"
-        PRACTICE = "PRA", "Практическое занятие"
-        ON_MANUFACTURE = "MAN", "На производстве"
-
-    kind = models.CharField(max_length=3, choices=Kind, null=False, blank=False)
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    kind = models.ForeignKey(EventKind, on_delete=models.PROTECT)
+    subject = models.ForeignKey(Subject, on_delete=models.PROTECT)
     participants = models.ManyToManyField(EventParticipant)
 
 
@@ -86,21 +87,27 @@ class EventHolding(CommonModel):
         verbose_name = "Информация о проведении события"
         verbose_name_plural = verbose_name
 
-    place = models.ForeignKey(EventPlace, null=True, on_delete=models.SET_NULL)
+    place = models.ForeignKey(EventPlace, null=True, on_delete=models.PROTECT)
     date = models.DateField(blank=False)
-    slot = models.ForeignKey(TimeSlot, null=True, on_delete=models.SET_NULL)
+    slot = models.ForeignKey(TimeSlot, null=True, on_delete=models.PROTECT)
     event = models.ForeignKey(Event, null=True, on_delete=models.SET_NULL)
 
 
 class Schedule(CommonModel):
+    class Meta:
+        verbose_name = "Расписание"
+        verbose_name_plural = "Расписания"
+
     class Scope(models.TextChoices):
-        BACHELOR = "BACHELOR", "Бакалавриат"
-        MASTER = "MASTER", "Магистрант"
-        POSTGRADUATE = "POSTGRADUATE", "Аспирантура"
-        CONSULTATION = "CONSULTATION", "Консультация"
+        BACHELOR = "bachelor", "Бакалавриат"
+        MASTER = "master", "Магистратура"
+        POSTGRADUATE = "postgraduate", "Аспирантура"
+        CONSULTATION = "consultation", "Консультация"
     
-    faculty = models.CharField(max_length=32)
-    scope = models.CharField(choices=Scope, max_length=32)
-    # TODO: Возможно сделать такой формат названия (проверка правильности по regex): ФЭВТ,2024-2025,бак,2курс,1с
-    name = models.CharField(max_length=128, verbose_name="Название расписания")
-    events = models.ManyToManyField(Event)
+    faculty = models.CharField(max_length=32, verbose_name="Факультет")
+    scope = models.CharField(choices=Scope, max_length=32, verbose_name="Обучение")
+    course = models.IntegerField(verbose_name="Курс")
+    semester = models.IntegerField(verbose_name="Семестр")
+    course_start = models.DateField(verbose_name="Начало учебного года")
+    course_finish = models.DateField(verbose_name="Окончание учебного года")
+    events = models.ManyToManyField(Event, verbose_name="Занятия")
